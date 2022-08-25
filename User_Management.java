b0ceaa7590c5ffaeff29f2bbcaf1788394f88b64
김선우 <72905532+sunwookim05@users.noreply.github.com>
2022-08-26 00:09:31 +0900

    Delete User_Management.java

--- User_Management.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class User_Management{
-    private Scanner sc = new Scanner(System.in);
-    public void SignUp(String id, String password, int money){
-        
-    }
-}
